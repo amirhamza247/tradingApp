2195a0f7c4db8ead06b9a578d13ac2ac1afc9a15
amir247 <amir_hamza@outlook.com>
2025-09-29 11:09:08 +0200

    adding my special utility methdos in progam.cs person.cs and items.cs.
    Because it becomes easier to write code and easy to read.

--- Person.cs
+++ Person.cs
@@ -2,6 +2,41 @@ namespace App;
 
 public class Person : IUser
 {
+  //////// MY SPECIAL METHODS //////////
+
+  //this methods converts Readline and Writeline to Print and Input.
+  static void print(string input)
+  {
+    Console.WriteLine(input);
+  }
+  static string input()
+  {
+    return Console.ReadLine() ?? "";
+  }
+
+
+  //this method gives text in console color.
+  static void paint(ConsoleColor color, string input, string newline = "newline")
+  {
+    if (newline == "newline")
+    {
+      Console.ForegroundColor = color;
+      print(input);
+      Console.ResetColor();
+    }
+    if (newline == "sameline")
+    {
+      Console.ForegroundColor = color;
+      Console.Write(input);
+      Console.ResetColor();
+    }
+  }
+
+  //this is to help vs code with debugging.
+  // try { Console.Clear(); } catch { Console.WriteLine("\n------------------\n");}
+
+  /////////^^^^^^^^^ MY SPECIAL METHODS ^^^^^^^^^//////////
+
   public string Email;
   public string _Password;
 
--- Program.cs
+++ Program.cs
@@ -1,1 +1,39 @@
 ﻿using App;
+
+//////// MY SPECIAL METHODS //////////
+
+//this methods converts Readline and Writeline to Print and Input.
+static void print(string input)
+{
+  Console.WriteLine(input);
+}
+static string input()
+{
+  return Console.ReadLine() ?? "";
+}
+
+
+//this method gives text in console color.
+static void paint(ConsoleColor color, string input, string newline = "newline")
+{
+  if (newline == "newline")
+  {
+    Console.ForegroundColor = color;
+    print(input);
+    Console.ResetColor();
+  }
+  if (newline == "sameline")
+  {
+    Console.ForegroundColor = color;
+    Console.Write(input);
+    Console.ResetColor();
+  }
+}
+
+//this is to help vs code with debugging.
+// try { Console.Clear(); } catch { Console.WriteLine("\n------------------\n");}
+
+/////////^^^^^^^^^ MY SPECIAL METHODS ^^^^^^^^^//////////
+
+List<IUser> allUsers = new List<IUser> { };
+
